0a096c33111cd14b69e9278eec9ed2e0ec44f680
yara <yaraharby4@gmail.com>
2025-05-08 19:00:41 +0300

    Restored ForgetPasswordRequest.java

---
+++ students/src/main/java/com/intern_project/students/dto/ForgetPasswordRequest.java
@@ -0,0 +1,9 @@
+package com.intern_project.students.dto;
+
+public class ForgetPasswordRequest {
+    private String email;
+
+    // Getters and Setters
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
